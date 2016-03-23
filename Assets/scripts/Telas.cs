076827e9ce88fa3ece2e25000565ea2d13c6287b
alexeu26 <alex.eu26@gmail.com>
2016-03-23 10:58:38 -0300

    Telas criadas

---
+++ Assets/scripts/Telas.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+public class Telas : MonoBehaviour {
+	public AudioSource som;
+
+	public AudioClip botaoIniciar;
+	public AudioClip botaoCredito;
+	public AudioClip botaoVoltar;
+
+	void Update () {
+	
+	}
+	public void iniciar(){
+		SceneManager.LoadSceneAsync ("Jogo");
+		som.PlayOneShot (botaoIniciar);
+		
+	}
+	public void creditos(){
+		SceneManager.LoadSceneAsync ("Credito");
+		som.PlayOneShot (botaoCredito);
+
+	}
+	public void voltar(){
+		SceneManager.LoadSceneAsync ("TelaPrincipal");
+		som.PlayOneShot (botaoVoltar);
+
+	}
+}
